15cb5a28283227ed9d73159a3aab31e2fc2abfd2
Ayush Tibrewal <aayushaayush107@gmail.com>
2025-04-10 14:18:53 +0530

    first source code

---
+++ starter-template/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+//importing fonts form nextjs 
+import { Inter  , Calistoga} from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge"; // or 'tailwind-merge' if you're using that
+
+
+const inter = Inter({ subsets: ["latin"] , variable: "--font-sans" });
+const calistoga = Calistoga({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-serif",
+});
+
+export const metadata: Metadata = {
+  title: "My Portfolio",
+  description: "Created with the help of Frontend Tribe",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={twMerge(`${inter.variable} ${calistoga.variable} bg-gray-900 text-white antialiased font-sans`)}>
+      {children}
+      </body>
+      </html>
+  );
+}
